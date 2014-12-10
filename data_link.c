69fa74644dd8db02c23ad1fef0893b57e35f0b00
Ben McMorran <ben.mcmorran@gmail.com>
2014-12-10 16:21:35 -0500

    Add basic data link layer implementation

--- data_link.c
+++ data_link.c
@@ -1,26 +1,44 @@
 #include "data_link.h"
 
-int neverCorrupt(int dataFrames, int ackFrames, enum FrameType frameType) {
+int neverCorrupt(int dataFrames, int ackFrames, FrameType frameType) {
 	return 0;
 }
 
-int (*shouldCorrupt)(int, int, enum FrameType) = neverCorrupt;
+int (*SHOULD_CORRUPT)(int, int, FrameType) = neverCorrupt;
 int dataFrameCount = 0;
 int ackFrameCount = 0;
-unit_16 sequence = 0;
+uint16_t sequence = 0;
 
-void dat_setShouldCorrupt(int (*corrupt)(int, int, enum FrameType frameType)) {
-	shouldCorrupt = corrupt;
+void dat_setShouldCorrupt(int (*corrupt)(int, int, FrameType frameType)) {
+	SHOULD_CORRUPT = corrupt;
+}
+
+void dat_error(char *data, size_t length, char *error) {
+	int i;
+	for (i = 0; i < length; i += 2)
+		error[0] ^= data[i];
+	for (i = 1; i < length; i += 2)
+		error[1] ^= data[i];
 }
 
 void dat_send(char *data, size_t length) {
-	// Stub for now
-	char error[2];
-	phy_send(data, length, error, shouldCorrupt(dataFrameCount, ackFrameCount, DATA));
+	char error[2] = { 0, 0 };
+	dat_error(data, length, error);
+	phy_send(data, length, error, SHOULD_CORRUPT(dataFrameCount, ackFrameCount, DATA));
 }
 
 size_t dat_recv(char *data, size_t length) {
-	// Stub for now
-	char buffer[100];
-	return phy_recv(data, 100);
+	char buffer[200];
+	size_t frameLength = phy_recv(buffer, 200);
+
+	char *frameError = (char *) buffer + frameLength - 2;
+	char expectedError[] = { 0, 0 };
+	dat_error(buffer, frameLength - 2, frameError);
+
+	if (frameError[0] != expectedError[0] || frameError[1] != expectedError[1])
+		error_user("dat_recv()", "error detection field invalid");
+
+	memcpy(data, buffer, frameLength - 2);
+
+	return frameLength - 2;
 }
--- data_link.h
+++ data_link.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "physical.h"
 #include "error.h"
 
 typedef enum {
@@ -7,8 +8,7 @@ typedef enum {
 	ACK
 } FrameType;
 
-extern int (*shouldCorrupt)(int, int, enum FrameType);
-void dat_setShouldCorrupt(int (*corrupt)(int, int, enum FrameType));
+void dat_setShouldCorrupt(int (*corrupt)(int, int, FrameType));
 
 void dat_send(char *data, size_t length);
 size_t dat_recv(char *data, size_t length);
