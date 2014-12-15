50a98e14e8ed1bbc07f572195cd5cd8a173d4cd3
Ben McMorran <ben.mcmorran@gmail.com>
2014-12-15 11:40:33 -0500

    Add logging functions, not yet instrumented

--- error.c
+++ error.c
@@ -25,3 +25,90 @@ void error_log_user(const char *msg, const char *detail) {
   fputs(detail, logfile);
   fputs("\n\n", logfile);
 }
+
+/* Author: Ben McMorran
+ * Logs the given message. */
+void log_msg(const char *msg) {
+  fputs(msg, logfile);
+  fputs("\n\n", logfile);
+}
+
+/* Author: Ben McMorran
+ * Logs that the given packet was sent. */
+void log_packetSent(int packet) {
+  char buffer[50];
+  sprintf(buffer, "NET: packet %d sent", packet);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that an ACK packet was sent. */
+void log_ackPacketSent() {
+  log_msg("NET: ACK packet sent");
+}
+
+/* Author: Ben McMorran
+ * Logs that an ACK packet was received. */
+void log_ackPacketReceived() {
+  log_msg("NET: ACK packet received");
+}
+
+/* Author: Ben McMorran
+ * Logs that the given frame was sent. */
+void log_frameSent(int packet, int frame) {
+  char buffer[50];
+  sprintf(buffer, "DAT: frame %d of packet %d sent", frame, packet);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that the given frame was resent. */
+void log_frameResent(int packet, int frame) {
+  char buffer[50];
+  sprintf(buffer, "DAT: frame %d of packet %d resent", frame, packet);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that the given frame was received. */
+void log_frameReceived(int packet, int frame) {
+  char buffer[50];
+  sprintf(buffer, "DAT: frame %d of packet %d received", frame, packet);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that a duplicate frame was received. */
+void log_duplicateFrameReceived(int packet, int frame) {
+  char buffer[70];
+  sprintf(buffer, "DAT: duplicate of frame %d of packet %d received", frame, packet);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that the given ACK frame was sent. */
+void log_ackFrameSent(int frame) {
+  char buffer[50];
+  sprintf(buffer, "DAT: ACK for sequence %d sent", frame);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that the given ACK frame was successfully received. */
+void log_ackFrameReceived(int frame) {
+  char buffer[70];
+  sprintf(buffer, "DAT: ACK for sequence %d successfully received", frame);
+  log_msg(buffer);
+}
+
+/* Author: Ben McMorran
+ * Logs that a frame was received with errors. */
+void log_frameError() {
+  log_msg("DAT: frame received in error");
+}
+
+/* Author: Ben McMorran
+ * Logs that a timeout occured. */
+void log_timeout() {
+  log_msg("DAT: timer expired");
+}
--- error.h
+++ error.h
@@ -10,3 +10,17 @@ FILE* logfile;
 void error_user(const char *msg, const char *detail);
 void error_system(const char *msg);
 void error_log_user(const char *msg, const char *detail);
+
+// Logging fuction prototypes. See error.c for full documentation.
+void log_msg(const char *msg);
+void log_packetSent(int packet);
+void log_ackPacketSent();
+void log_ackPacketReceived();
+void log_frameSent(int packet, int frame);
+void log_frameResent(int packet, int frame);
+void log_frameReceived(int packet, int frame);
+void log_duplicateFrameReceived(int packet, int frame);
+void log_ackFrameSent(int frame);
+void log_ackFrameReceived(int frame);
+void log_frameError();
+void log_timeout();
